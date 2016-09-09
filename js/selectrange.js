3133ba9843bb9a305d51862e20e8501988a63736
Nagesh <nsangana@traveltripper.com>
2016-09-09 15:37:57 -0400

    booking widget changes

---
+++ js/selectrange.js
@@ -0,0 +1,26 @@
+$(function () {
+
+	$('.multiple').pickmeup({
+		flat	: false,
+		mode	: 'multiple'
+	});
+	$('.range').pickmeup({
+		flat	: true,
+		mode	: 'range'
+	});
+	var plus_5_days	= new Date;
+	plus_5_days.addDays(0);
+	$('.3-calendars').pickmeup({
+		flat		: true,
+		date		: [
+			new Date,
+			plus_5_days
+		],
+		mode		: 'range',
+		calendars	: 3
+	});
+	$('input').pickmeup({
+		position		: 'right',
+		hide_on_select	: true
+	});
+});
