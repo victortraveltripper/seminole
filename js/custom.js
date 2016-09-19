6ae845c72526181f2779a87f188eae1a3379c0cf
kiran-TT-web <kmekala@traveltripper.com>
2016-09-19 19:44:14 +0530

    datepicker

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,14 @@ $(document).ready(function(){
 
 
 
+
+  //$('#sandbox-container div').datepicker({
+
+  //});
+
+
+
+
   $("#GallerySlider").owlCarousel({
 
 
@@ -153,5 +161,40 @@ $(document).ready(function(){
                    e.preventDefault();
                }
        });
+
+
+
+       ///date picker
+
+       $(".datepicker").datepicker({
+   			minDate: 0,
+   			numberOfMonths: [4,1],
+   			beforeShowDay: function(date) {
+   				var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").val());
+   				var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").val());
+   				return [true, date1 && ((date.getTime() == date1.getTime()) || (date2 && date >= date1 && date <= date2)) ? "dp-highlight" : ""];
+   			},
+   			onSelect: function(dateText, inst) {
+   				var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").val());
+   				var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").val());
+                   var selectedDate = $.datepicker.parseDate($.datepicker._defaults.dateFormat, dateText, "DD, d MM, yy");
+
+
+                   if (!date1 || date2) {
+   					$("#input1").val(dateText);
+   					$("#input2").val("");
+                       $(this).datepicker();
+                   } else if( selectedDate < date1 ) {
+                       $("#input2").val( $("#input1").val() );
+                       $("#input1").val( dateText );
+                       $(this).datepicker();
+                   } else {
+   					$("#input2").val(dateText);
+                       $(this).datepicker();
+   				}
+   			}
+   		});
+
+
 
 });
