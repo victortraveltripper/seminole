b19499c3ed754834cbaa119d6cc9e5c4ef73546d
tt-admin <admin@traveltripper.io>
2016-09-20 13:13:56 +0200

    Update Plugins

--- _plugins/i18n_front_matter.rb
+++ _plugins/i18n_front_matter.rb
@@ -4,5 +4,10 @@ Jekyll::Hooks.register :site, :post_read do |site|
 
   site.pages.each do |page|
     page.data = translate_data(page.data, locale, default_locale)
+    permalink = page.data['permalink']
+    if permalink && !(permalink.end_with?('/') || permalink.end_with?('.html'))
+      p "Permalink #{permalink} is invalid. Must ends with '/' or html extension"
+      page.data['permalink'] += '.html'
+    end
   end
 end
