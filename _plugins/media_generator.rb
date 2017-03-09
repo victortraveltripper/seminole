acc8eb7dd75b522369309c5b111ff387da10421f
tt-admin <tt-admin@users.noreply.github.com>
2017-03-09 13:13:49 -0500

    Update Plugins

--- _plugins/media_generator.rb
+++ _plugins/media_generator.rb
@@ -10,11 +10,11 @@ module Jekyll
   class MediaGenerator < Generator
     # This generator is safe from arbitrary code execution.
     safe true
+    
     def generate(_site)
-      create_json_files media_dir, 'media' # creates image_data.json
-      create_old_media old_media_dir # creates old_media.json
-      create_definition_files definitions_dir # creates models.json
-      create_json_files model_dir, 'models' # creates models.json
+      create_media_files  # creates image_data.json
+      create_old_media # creates old_media.json
+      create_model_files
     end
 
     private
@@ -34,78 +34,76 @@ module Jekyll
     def old_media_dir
       File.join('_assets', 'images')
     end
-    
+
     def absolute_dir(dir)
       File.expand_path(File.join(Dir.pwd, dir))
     end
 
-    # save the contnet in a file along with .json format
-    def save(filename, content)
-      File.open("#{filename}.json", 'w') do |f|
-        f.write(JSON.pretty_generate(content))
+    def save_json(filename, content)
+      File.open("#{filename}.json", 'w') do |file|
+        file.write(JSON.pretty_generate(content))
       end
     end
-    
+
     def clean_file(filename)
-      `rm #{filename}.json`
+      `rm #{filename}.json` if File.exist?("#{filename}.json")
     end
-    
 
     # create old_media json for the images fetching from Github
-    def create_old_media(folder)
+    def create_old_media
+      folder = old_media_dir
       clean_file('old_media')
-      folder = absolute_dir(folder)
-      return [] unless File.directory? folder
+      abs_folder = absolute_dir(folder)
+      return [] unless File.directory? abs_folder
       old_media_data = []
-      Dir[File.join(folder, '/*')].each do |file|
+      Dir[File.join(abs_folder, '/*')].each do |file|
         old_media_data << { :path => File.basename(file), :sha => '' }
       end
-      save 'old_media', old_media_data
+      save_json 'old_media', old_media_data
     end
 
-    # creates model.json file by reading all definitions.
-    def create_definition_files(folder)
-      clean_file('models')
-      folder = absolute_dir(folder)
-      return unless File.directory? folder
-      hash = Hash.new { |h, k| h[k] = [] }
-      Dir.glob("#{definitions_dir}/**/*.json").map do |f|
-        filename = File.basename(f, '.*').to_s
-        hash[filename] << [name: '', file: '']
-      end
-      save 'models', hash
-    end
-
-    # creates model.json if definitions and _models directory exists.
-    #    else this function will skip to create model.json.
-    #  creates media file based on params passing.
-
-    def create_json_files(folder, file_name = 'models')
+    def create_media_files
+      folder = media_dir
+      file_name = 'media'
       clean_file(file_name)
       abs_folder = absolute_dir(folder)
       return unless File.directory? abs_folder
-      sub_folders = Dir.entries("#{abs_folder}/").select { |entry| File.directory? File.join(abs_folder, entry) and !(entry == '.' || entry == '..') }
-      if sub_folders.empty?
-        writing_file_data = Dir[File.join(abs_folder, '*.json')].map do |f| 
-          data = JSON.parse File.read(f) 
-          data[:git_file_name] = File.join(folder, File.basename(f))
-          data
-        end.flatten
-        save file_name, writing_file_data
-      else
-        hash = Hash.new { |h, k| h[k] = [] }
-        sub_folders.each do |sub_folder|
-          Dir[File.join(abs_folder, sub_folder, '*.json')].map do |f|
-            data = JSON.parse File.read(f)
-            attrs = {}
-            attrs[:name] = data['name'] if data.has_key?('name')
-            attrs[:title] = data['title'] if data.has_key?('title')
-            attrs[:file] = File.basename(f)
-            hash[sub_folder.to_s] << [attrs]
-          end
-        end
-        save file_name, hash
+      writing_file_data = Dir[File.join(abs_folder, '*.json')].map do |file|
+        data = JSON.parse File.read(file)
+        data[:git_file_name] = File.join(folder, File.basename(file))
+        data
+      end.flatten
+      save_json file_name, writing_file_data
+    end
+
+    def create_model_files
+      file_name = 'models'
+      clean_file(file_name)
+      def_abs_folder = absolute_dir(definitions_dir)
+      # If there is not a definitions directory, we have no models
+      return unless File.directory? def_abs_folder
+      
+      # First get all of the definitions
+      models_hash = Hash.new { |h, k| h[k] = [] }
+      Dir[File.join(def_abs_folder, '*.json')].each do |f| 
+        models_hash[File.basename(f).gsub('.json','')] = []        
       end
+      
+      # Then load basic instance data into the definition instance lists
+      models_abs_folder = absolute_dir(model_dir)
+      models_sub_folders = Dir.entries("#{models_abs_folder}/").select { |entry| File.directory? File.join(models_abs_folder, entry) and !(entry == '.' || entry == '..') }
+      
+      models_sub_folders.each do |sub_folder|
+        Dir[File.join(models_abs_folder, sub_folder, '*.json')].map do |f|
+          data = JSON.parse File.read(f)
+          attrs = {}
+          attrs[:name] = data['name'] if data.has_key?('name')
+          attrs[:title] = data['title'] if data.has_key?('title')
+          attrs[:file] = File.basename(f)
+          models_hash[sub_folder.to_s] << [attrs]
+        end
+      end
+      save_json file_name, models_hash
     end
   end
 end
